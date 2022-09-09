9aa1a16f352957cadac735dd54b066870027dafe
cemilehadzhieva <cemilehadzhieva@gmail.comm>
2022-09-10 00:45:54 +0300

    odev

---
+++ src/ödev/ödev1.java
@@ -0,0 +1,30 @@
+package ödev;
+
+import java.util.Scanner;
+
+public class ödev1 {
+    public static void main(String[] args) {
+        int kilo;
+        double boy;
+        Scanner inp= new Scanner(System.in);
+
+        System.out.print("Kilonuz:");
+        kilo = inp.nextInt();
+
+        System.out.print("Boyunuz:");
+        boy = inp.nextDouble();
+
+
+
+        double sonuc= (double) kilo/( boy*boy);
+        System.out.print("Vücüt Kitle İndeksiniz :" + sonuc);
+
+
+
+
+
+
+
+
+    }
+}
