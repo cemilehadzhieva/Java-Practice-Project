f2146b4a01605914212f01dfdcb2b08a38508ab3
cemilehadzhieva <cemilehadzhieva@gmail.comm>
2022-09-11 15:35:36 +0300

    daire

---
+++ src/ödev/daire.java
@@ -0,0 +1,30 @@
+package ödev;
+import java.util.Scanner;
+
+
+
+public class daire {
+    public static void main(String[] args) {
+       int r,a;
+       double pi = 3.14;
+
+
+       Scanner inp = new Scanner(System.in);
+       System.out.println("Dairenin Yarıçapını giriniz : ");
+       Scanner inpo = new Scanner(System.in);
+       System.out.println("Merkez açısnın ölçüsünü girirniz : ");
+
+       r = inp.nextInt();
+       a = inp.nextInt();
+       double alan = (pi * (r*r) * a)/360;
+
+
+       System.out.println("Daire diliminin alanı : "+ alan);
+
+
+
+
+
+
+    }
+}
