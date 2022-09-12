1d44ccfe8f576ec37f04f328534c776097d4f743
cemilehadzhieva <cemilehadzhieva@gmail.comm>
2022-09-12 17:18:04 +0300

    kullanıcı girişi

---
+++ src/ödev/kullanıcıgirisi.java
@@ -0,0 +1,49 @@
+package ödev;
+import java.util.Scanner;
+
+public class kullanıcıgirisi {
+    public static void main(String[] args) {
+        String userName,password;
+
+
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Kullanıcı Adınız :");
+        userName = inp.nextLine();
+
+        System.out.println("Şifreniz :");
+        password = inp.nextLine();
+
+        if (userName.equals("patika")){
+            if (password.equals("java123")){
+                System.out.println("Giriş Yaptınız !");
+            }else{
+                System.out.println("Şifenizi sıfırlamak ister misiniz?");
+
+                String verify = inp.nextLine();
+                System.out.println(verify);
+
+                if (verify.equals("evet")){
+                    System.out.println("Yeni şifrenizi girin :");
+                    String oldPass = password;
+                    password = inp.next();
+                    if (!oldPass.equals(password)&& !password.equals("java123")){
+                        System.out.println("Şifreniz oluşturuldu.");
+                    }else{
+                        System.out.println("Şifreniz oluşturulamadı. Lütfen başka şifre giriniz.");
+                    }
+                }
+            }
+        }else {
+            System.out.println("Bilgileriniz yanlış");
+        }
+
+        }
+
+
+
+
+
+
+}
