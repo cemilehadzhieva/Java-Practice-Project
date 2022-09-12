017b78c218918575231d5e7f73379caa5d4b0cec
cemilehadzhieva <cemilehadzhieva@gmail.comm>
2022-09-12 21:19:15 +0300

    etkinlik

---
+++ src/ödev/etkinlik.java
@@ -0,0 +1,33 @@
+package ödev;
+import java.util.Scanner;
+
+
+public class etkinlik {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sıcaklık Giriniz :");
+        heat= input.nextInt();
+
+        if (heat < 5){
+            System.out.println("Kayak yapabilirsiniz");
+
+        } else if (heat>=5  && heat <= 25) {
+            if (heat <= 15){
+                System.out.println("Sinemaya gidebilirsin.");
+            }
+            if (heat>= 10){
+                System.out.println("Pikniğe gideblirsiniz.");
+
+            }
+
+        } else if (heat> 25) {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+
+        }
+
+
+    }
+    }
+
