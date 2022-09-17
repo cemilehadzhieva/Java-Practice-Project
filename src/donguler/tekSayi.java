6972bba0483fc2808e9482bdf37bbe14c3d6a329
cemilehadzhieva <cemilehadzhieva@gmail.comm>
2022-09-17 16:37:02 +0300

    tekSayi

--- src/donguler/tekSayi.java
+++ src/donguler/tekSayi.java
@@ -3,15 +3,22 @@ import java.util.Scanner;
 
 public class tekSayi {
     public static void main(String[] args) {
-       /*
+
        int n;
+       int total = 0;
        Scanner scan = new Scanner(System.in);
 
        do {
-           System.out.println("Sayı Giriniz: ");
+           System.out.print("Sayı Giriniz: ");
            n = scan.nextInt();
+           if(n % 2 == 0 && n % 4 == 0){
+               total = total + n;
        }
 
-    } */
+       } while (n % 2 == 0);
+
+       System.out.println("Toplam : " + total);
+
+
     }
 }
